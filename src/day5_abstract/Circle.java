987c69f7bd0b4c770006d1c5f02a1694e548eb7e
unknown <tranthihoanhy@gmail.com>
2023-11-05 17:29:17 +0700

    delete old code

--- src/day5_abstract/Circle.java
+++
@@ -1,9 +0,0 @@
-package day5_abstract;
-
-public class Circle extends Shape{
-
-	@Override
-	public void draw() {
-		System.out.println("Vẽ hình tròn với màu "+ getColor()+ " bằng đường tròn");
-	}
-}
--- src/day5_abstract/Rectangle.java
+++
@@ -1,9 +0,0 @@
-package day5_abstract;
-
-public class Rectangle extends Shape{
-	
-	@Override
-	public void draw() {
-		System.out.println("Vẽ hình vuông với màu "+getColor()+ " bằng đường thẳng");
-	}
-}
